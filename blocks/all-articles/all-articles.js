57429451d9e956d08709aefdf8352e10970e2d44
gouriTD <gouri30@gmail.com>
2025-03-21 22:05:37 +0530

    Adding dynamic content for all articles

--- blocks/all-articles/all-articles.js
+++ blocks/all-articles/all-articles.js
@@ -19,5 +19,37 @@ export default async function decorate(block) {
     const allArticles = await fetchAllArticles();
     console.log(allArticles); 
 
+    [...block.querySelectorAll(':scope > div')].forEach(row=>{
+        [...row.querySelectorAll(':scope > div')].forEach(col =>{
+            const colDiv = col.closest('div');
+            if(!colDiv.hasChildNodes() && colDiv.textContent.trim() === ''){
+                colDiv.remove();
+            }
+        });
+
+        row.classList.add('articles-content-container');
+    });
+
+    const all_articles_container = block.querySelector('.articles-content-container');
+    const articleList = document.createElement('ul')
+    allArticles.forEach(article=>{
+        const articleItem = document.createElement('li');
+
+        // Testing for the tags from the json file.
+        const url = Array.from(JSON.parse(article.tags));
+        console.log(typeof url,url);
+    
+        articleItem.innerHTML = `
+            <a href=${article.path}>
+                <img src=${article.image}/>
+                <h3>${article.title}</h3>
+            </a>
+            <p>${article.description}</p>
+        `
+
+        articleList.append(articleItem);
+    });
+
+    all_articles_container.append(articleList);
 
 }
