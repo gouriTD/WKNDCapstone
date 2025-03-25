890c41aec8f1e31ed62bc610c4de08e5cdefc785
gouriTD <gouri30@gmail.com>
2025-03-25 11:35:09 +0530

    Resolving alignment with the signinBanner and card block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -10,6 +10,18 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+
+    // Now if the li element has an anchor tag.
+    // then we wrap the content of li in it.
+    if(li.querySelector(':scope a')) {
+      const anchorEl = document.createElement('a');
+      anchorEl.setAttribute('href',`${li.querySelector('.cards-card-body a').href}`)
+      li.querySelector('.cards-card-body >p >a').remove();
+      anchorEl.appendChild(li.querySelector('.cards-card-image'));
+      anchorEl.appendChild(li.querySelector('.cards-card-body'));
+      
+      li.append(anchorEl);
+    }
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
